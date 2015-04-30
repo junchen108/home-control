d4a8e3734a8c2652e082b5a530453b126613a463
Jun Chen <jun.chen108@outlook.com>
2015-04-30 20:43:41 +0200

    Created the new cercle button class

---
+++ HomeControl/HomeControl/LastValueButton.swift
@@ -0,0 +1,22 @@
+//
+//  LastValueButton.swift
+//  HomeControl
+//
+//  Created by Jun Chen on 30/04/15.
+//  Copyright (c) 2015 EPFL. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LastValueButton: UIButton {
+    
+    @IBInspectable var fillColor: UIColor = UIColor.greenColor()
+    
+    override func drawRect(rect: CGRect) {
+        var path = UIBezierPath(ovalInRect: rect)
+        fillColor.setFill()
+        path.fill()
+    }
+
+}
--- HomeControl/HomeControl/MainViewController.swift
+++ HomeControl/HomeControl/MainViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  HomeControl
 //
-//  Created by Jun Chen on 22/02/15.
+//  Created by Jun Chen on 30/04/15.
 //  Copyright (c) 2015 EPFL. All rights reserved.
 //
 
@@ -10,39 +10,26 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-    
-    let networkClient = NetworkClient(serverUrlString: "http://localhost:9000/environment_data")
-        
-    @IBOutlet weak var temperatureDisplay: UILabel!
-    
-    @IBOutlet weak var dateDisplay: UILabel!
-    
-    @IBAction func displayEnvironmentData() {
-        networkClient.fetchData() {
-            (data, error) -> Void in
-            if error != nil {
-                println("ERROR")
-            } else {
-                println(data)
-                let lastEnvironmentInfo = JsonEnvironmentParser.convertLastDataToEnvironment(data)
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.temperatureDisplay.text = lastEnvironmentInfo.temperature
-                    self.dateDisplay.text = lastEnvironmentInfo.date
-                })
-            }
-        }
+        // Do any additional setup after loading the view.
+    }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-}
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
